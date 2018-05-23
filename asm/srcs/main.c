10b4411a79e28883b04fb63b40f13db803167787
eparisot <eparisot@student.42.fr>
2018-05-23 03:02:19 +0200

    MSG

--- asm/srcs/main.c
+++ asm/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: pmilan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/17 20:07:51 by pmilan            #+#    #+#             */
-/*   Updated: 2018/05/23 00:24:43 by eparisot         ###   ########.fr       */
+/*   Updated: 2018/05/23 03:02:02 by eparisot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,21 @@ int		main(int argc, char **argv)
 	int			argc_c;
 
 	argc_c = argc;
-	while (--argc && ft_strstr(argv[argc], ".s"))
+	while (--argc)
 	{
-		if (!(champ = (t_champ*)malloc(sizeof(t_champ))))
-			return (0);
-		ft_init_champ(champ);
-		if (read_file(argv[argc], champ) == ERROR)
+		if (ft_strstr(argv[argc], ".s"))
 		{
+			if (!(champ = (t_champ*)malloc(sizeof(t_champ))))
+				return (0);
+			ft_init_champ(champ);
+			if (read_file(argv[argc], champ) == ERROR)
+			{
+				del_champ(champ);
+				exit(EXIT_FAILURE);
+			}
+			ft_write_cor(champ, argv[argc]);
 			del_champ(champ);
-			exit(EXIT_FAILURE);
 		}
-		ft_write_cor(champ, argv[argc]);
-		del_champ(champ);
 	}
 	if (argc_c == 1)
 		ft_printf("{yellow}Usage: %s <sourcefile.s>{eoc}\n", argv[0]);
