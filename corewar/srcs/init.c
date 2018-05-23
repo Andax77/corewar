2dfac7d7bb8c25bf950f89082f5c798c53972983
eparisot <eparisot@student.42.fr>
2018-05-23 18:12:33 +0200

    normed ncurse

--- corewar/srcs/init.c
+++ corewar/srcs/init.c
@@ -6,22 +6,19 @@
 /*   By: eparisot <eparisot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/21 18:11:42 by eparisot          #+#    #+#             */
-/*   Updated: 2018/05/23 17:25:04 by eparisot         ###   ########.fr       */
+/*   Updated: 2018/05/23 18:12:25 by eparisot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <corewar.h>
 
-int		init_ncurse()
+static void	draw_borders(void)
 {
 	int		x;
 	int		y;
 
 	x = 0;
 	y = 0;
-	initscr();
-	start_color();
-	init_color(COLOR_CYAN, 500, 500, 500);
 	init_pair(1, COLOR_CYAN, COLOR_CYAN);
 	attron(COLOR_PAIR(1));
 	while (y < 68)
@@ -32,9 +29,15 @@ int		init_ncurse()
 		if (x > 254 && !(x = 0))
 			y++;
 	}
+}
+
+static void	draw_map(void)
+{
+	int		x;
+	int		y;
+	
 	x = 3;
 	y = 2;
-	refresh();
 	init_pair(2, COLOR_CYAN, COLOR_BLACK);
 	attron(COLOR_PAIR(2));
 	while (y < 66)
@@ -44,17 +47,23 @@ int		init_ncurse()
 		if (x >= 194 && (x = 3))
 			y++;
 	}
+}
+
+static void		init_ncurse(void)
+{
+	initscr();
+	start_color();
+	init_color(COLOR_CYAN, 500, 500, 500);
+	draw_borders();
+	draw_map();
 	curs_set(0);
-	return (SUCCESS);
 }
 
 int		init(t_opt *opt)
 {
 	if (opt->n)
 		if (!init_ncurse())
-		{
 			return (ERROR);
-		}
 	getch();
 	endwin();
 	return (SUCCESS);
