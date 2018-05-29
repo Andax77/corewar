42f0008316c324d0cbcf74d02faebe742e3888a3
Pierre MILAN <pmilan@e2r13p6.42.fr>
2018-05-29 16:17:44 +0200

    debut bonus

--- asm/srcs/main.c
+++ asm/srcs/main.c
@@ -6,34 +6,40 @@
 /*   By: pmilan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/17 20:07:51 by pmilan            #+#    #+#             */
-/*   Updated: 2018/05/24 22:31:48 by pmilan           ###   ########.fr       */
+/*   Updated: 2018/05/29 15:58:10 by pmilan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <asm.h>
 
-int		main(int argc, char **argv)
+int				main(int argc, char **argv)
 {
 	t_champ		*champ;
 	int			argc_c;
 
 	argc_c = argc;
-	while (--argc)
+	while (--argc_c)
 	{
-		if (!ft_strequ(argv[argc], "-r"))
+		champ = ft_malloc(sizeof(t_champ), EXIT_FAILURE);
+		ft_init_champ(champ, argv[argc_c]);
+		if (ft_strcmp("-r", argv[1]) == 0)
 		{
-			champ = ft_malloc(sizeof(t_champ), EXIT_FAILURE);
-			ft_init_champ(champ, argv[argc]);
-			if (ft_fill_file_name(champ, argv[argc]) == ERROR)
+			if (argc_c == 1)
 				continue ;
-			if (read_file(argv[argc], champ) == ERROR)
+
+		}
+		else
+		{
+			if (ft_fill_file_name(champ, argv[argc_c]) == ERROR)
+				continue ;
+			if (read_file(argv[argc_c], champ) == ERROR)
 				continue ;
 			if (ft_write_cor(champ) == ERROR)
 				continue ;
-			del_champ(champ);
 		}
+		del_champ(champ);
 	}
-	if (argc_c == 1)
+	if (argc == 1)
 		ft_printf("{yellow}Usage: %s <sourcefile.s>{eoc}\n", argv[0]);
 	exit(EXIT_SUCCESS);
 }
