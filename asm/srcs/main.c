f39f40ea7e9ebcc2ebd1de6b3359d5815ea7fc7c
Pierre MILAN <pmilan@e2r13p6.42.fr>
2018-05-29 17:28:49 +0200

    usage

--- asm/srcs/main.c
+++ asm/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: pmilan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/17 20:07:51 by pmilan            #+#    #+#             */
-/*   Updated: 2018/05/29 15:58:10 by pmilan           ###   ########.fr       */
+/*   Updated: 2018/05/29 17:26:38 by pmilan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,6 @@ int				main(int argc, char **argv)
 		del_champ(champ);
 	}
 	if (argc == 1)
-		ft_printf("{yellow}Usage: %s <sourcefile.s>{eoc}\n", argv[0]);
+		ft_printf("{yellow}Usage: %s [-r] <sourcefile> ...{eoc}\n", argv[0]);
 	exit(EXIT_SUCCESS);
 }
