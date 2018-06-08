916d1d3213ead97905c480fe9accb9bdf1d36c05
eparisot <eparisot@student.42.fr>
2018-06-08 19:37:11 +0200

    champs verified OK

--- corewar/srcs/parse_champ.c
+++ corewar/srcs/parse_champ.c
@@ -6,7 +6,7 @@
 /*   By: eparisot <eparisot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/25 23:28:10 by eparisot          #+#    #+#             */
-/*   Updated: 2018/06/07 20:22:11 by eparisot         ###   ########.fr       */
+/*   Updated: 2018/06/08 19:33:03 by eparisot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -117,14 +117,13 @@ static int		check_champ_bis(t_list *instru, t_champ **champ, char *path)
 	split_bits(&(*champ)->prog, &(*champ)->splited_prog);
 	if (!((*champ)->reg = ft_memalloc(16 * sizeof(unsigned int))))
 		exit(EXIT_FAILURE);
-	//TODO Check prog (operations conformity)
-	//if (check_op_len(*champ) == ERROR)
-	//{
-	//	ft_printf("{red}Invalid operation in %s\n{eoc}", path);
-	//	return (ERROR);
-	//}
+	if (check_op_len(*champ) == ERROR)
+	{
+		ft_printf("{red}Invalid operation in %s\n{eoc}", path);
+		return (ERROR);
+	}
 	//DEBUG
-	ft_printf("champ name = %s, op_nb = %d,	 comment = %s, prog = %s, splited_prog[first] = %d, splited_prog[last] = %d, reg = %x\n", (*champ)->name, (*champ)->op_nb, (*champ)->comment, (*champ)->prog, (*champ)->splited_prog[0], (*champ)->splited_prog[(*champ)->op_nb - 1], (*champ)->reg[0]);
+	//ft_printf("champ name = %s, op_nb = %d,	 comment = %s, prog = %s, splited_prog[first] = %d, splited_prog[last] = %d, reg = %x\n", (*champ)->name, (*champ)->op_nb, (*champ)->comment, (*champ)->prog, (*champ)->splited_prog[0], (*champ)->splited_prog[(*champ)->op_nb - 1], (*champ)->reg[0]);
 	//
 	return (SUCCESS);
 }
--- corewar/srcs/tools2.c
+++ corewar/srcs/tools2.c
@@ -6,24 +6,97 @@
 /*   By: eparisot <eparisot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/07 19:55:19 by eparisot          #+#    #+#             */
-/*   Updated: 2018/06/07 20:21:29 by eparisot         ###   ########.fr       */
+/*   Updated: 2018/06/08 19:36:22 by eparisot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <corewar.h>
 
-int		check_op_len(t_champ *champ)
+static int	parse_op(unsigned char *op)
 {
 	int		i;
 
 	i = 0;
+	if (op[i + 1])
+	{
+		if (op[i + 1] == 0x54)
+			i += 5;
+		else if (op[i + 1] == 0xD4 || op[i + 1] == 0x74)
+			i += 6;
+		else if (op[i + 1] == 0xF4)
+			i += 7;
+		else if (op[i + 1] == 0x94 || op[i + 1] == 0x64)
+			i += 8;
+		else if (op[i + 1] == 0xB4 || op[i + 1] == 0xE4)
+			i += 9;
+		else if (op[i + 1] == 0xA4)
+			i += 11;
+	}
+	return (i);
+}
+
+static int	parse_op_bis(unsigned char *op)
+{
+	int		i;
+
+	i = 0;
+	if (op[i + 1])
+	{
+		if (op[i + 1] == 0x54)
+			i += 5;
+		else if (op[i + 1] == 0xD4 || op[i + 1] == 0x94 || op[i + 1] == 0x64)
+			i += 6;
+		else if (op[i + 1] == 0xE4 || op[i + 1] == 0xA4)
+			i += 7;
+	}
+	return (i);
+}
+
+static int	parse_op_ter(unsigned char *op)
+{
+	int		i;
+
+	i = 0;
+	if (op[i + 1])
+	{
+		if (op[i + 1] == 0x54)
+			i += 5;
+		else if (op[i + 1] == 0x74 || op[i + 1] == 0x64 || op[i + 1] == 0x58)
+		{
+			i += 6;
+		}
+		else if (op[i + 1] == 0x78 || op[i + 1] == 0x68)
+		{
+			i += 7;
+		}
+	}
+	return (i);
+}
+
+int			check_op_len(t_champ *champ)
+{
+	int				i;
+	unsigned char	*prog;
+
+	i = 0;
+	prog = champ->splited_prog;
 	while (i < champ->op_nb)
 	{
-		if (champ->splited_prog[i] > 16)
+		if (prog[i] > 16)
 			return (ERROR);
-		(champ->splited_prog[i] == 1) ? i += 4: 0;
-		//TODO all op_codes
-		i++;
+		(prog[i] == 1 || prog[i] == 4 || prog[i] == 5) ? i += 5 : 0;
+		((prog[i] == 2 || prog[i] == 13) && prog[i + 1] == 0xD0) ? i += 5 : 0;
+		((prog[i] == 2 || prog[i] == 13) && prog[i + 1] == 0x90) ? i += 7 : 0;
+		(prog[i] == 3 && prog[i + 1] == 0x50) ? i += 4 : 0;
+		(prog[i] == 3 && prog[i + 1] == 0x70) ? i += 5 : 0;
+		if (prog[i] == 6 || prog[i] == 7 || prog[i] == 8)
+			i += parse_op(&prog[i]);
+		if (prog[i] == 9 || prog[i] == 12 || prog[i] == 15 || prog[i] == 16)
+			i += 3;
+		if (prog[i] == 10 || prog[i] == 14)
+			i += parse_op_bis(&prog[i]);
+		if (prog[i] == 11)
+			i += parse_op_ter(&prog[i]);
 	}
 	return (SUCCESS);
 }
