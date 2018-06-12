0fcadb6874acd2e976703ac0512d1d590449779f
eparisot <eparisot@student.42.fr>
2018-06-12 16:04:13 +0200

    champ reg[0] ok

--- corewar/srcs/parse_champ.c
+++ corewar/srcs/parse_champ.c
@@ -6,7 +6,7 @@
 /*   By: eparisot <eparisot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/25 23:28:10 by eparisot          #+#    #+#             */
-/*   Updated: 2018/06/12 15:10:34 by eparisot         ###   ########.fr       */
+/*   Updated: 2018/06/12 16:03:51 by eparisot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -122,7 +122,8 @@ static int		check_champ_bis(t_list *instru, t_champ **champ, char *path)
 
 int				check_champ(t_champ **champ, char *path)
 {
-	t_list	*instru;
+	t_list		*instru;
+	static int	id;
 
 	if (!(instru = (*champ)->instru))
 	{
@@ -143,7 +144,7 @@ int				check_champ(t_champ **champ, char *path)
 		return (ERROR);
 	if (!((*champ)->reg = ft_memalloc(REG_NUMBER * sizeof(unsigned int))))
 		exit(EXIT_FAILURE);
-
+	((*champ)->reg)[0] = ++id;
 	//DEBUG
 	ft_printf("champ name = %s, op_nb = %d,	 comment = %s, prog = %s, splited_prog[first] = %d, splited_prog[last] = %d, reg = %x\n", (*champ)->name, (*champ)->op_nb, (*champ)->comment, (*champ)->prog, (*champ)->splited_prog[0], (*champ)->splited_prog[(*champ)->op_nb - 1], (*champ)->reg[0]);
 
