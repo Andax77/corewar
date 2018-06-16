e06145fddc152a8eb35d6b045003757108406dd7
eparisot <eparisot@student.42.fr>
2018-06-16 19:33:44 +0200

    splitting cycle

--- corewar/srcs/cycle.c
+++ corewar/srcs/cycle.c
@@ -6,7 +6,7 @@
 /*   By: anhuang <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/13 15:06:17 by anhuang           #+#    #+#             */
-/*   Updated: 2018/06/16 18:39:00 by eparisot         ###   ########.fr       */
+/*   Updated: 2018/06/16 19:32:58 by eparisot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,6 +55,80 @@ void	cycle_job(t_cor *cor, t_champ *cur_champ, int *last_champ, int *last_pc)
 	}
 }
 
+void	key_event(int *timeout, int *ch)
+{
+	char	*p_p_c;
+
+	if (*ch != ' ')
+	{
+		noecho();
+		*ch = getch();
+		while (*ch != 's' && *ch != ' ')
+		{
+			if (*ch == 'e' && *timeout > 0)
+			{
+				p_p_c = ft_itoa(1000 - --*timeout);
+				attron(COLOR_PAIR(7));
+				draw_line(4, 22, "    ");
+				draw_line(4, 22, p_p_c);
+				free(p_p_c);
+			}
+			else if (*ch == 'r' && *timeout > 1)
+			{
+				if (1000 - (*timeout - 10) > 1000)
+					p_p_c = ft_itoa(1000 - (*timeout = 0));
+				else
+					p_p_c = ft_itoa(1000 - (*timeout -= 10));
+				attron(COLOR_PAIR(7));
+				draw_line(4, 22, "    ");
+				draw_line(4, 22, p_p_c);
+				free(p_p_c);
+			}
+			else if (*ch == 'w' && *timeout < 950)
+			{
+				p_p_c = ft_itoa(1000 - ++*timeout);
+				attron(COLOR_PAIR(7));
+				draw_line(4, 22, "    ");
+				draw_line(4, 22, p_p_c);
+				free(p_p_c);
+			}
+			else if (*ch == 'q' && *timeout < 950)
+			{
+				if (1000 - (*timeout + 10) < 50)
+					p_p_c = ft_itoa(1000 - (*timeout = 950));
+				else
+					p_p_c = ft_itoa(1000 - (*timeout += 10));
+				attron(COLOR_PAIR(7));
+				draw_line(4, 22, "    ");
+				draw_line(4, 22, p_p_c);
+				free(p_p_c);
+			}
+			*ch = getch();
+		}
+		echo();
+	}
+	else
+	{
+		usleep(*timeout);
+		refresh();
+	}
+}
+
+void	print_infos(t_cor *cor)
+{
+	char	*cycle;
+
+	cycle = ft_itoa((cor->cycle)++);
+	attron(COLOR_PAIR(7));
+	draw_line(7, 9, cycle);
+	if (cor->cycle == 1)
+	{
+		attron(COLOR_PAIR(7));
+		draw_line(4, 22, "50");
+	}
+	free(cycle);
+}
+
 void	cycle(t_cor *cor)
 {
 	t_list		*champs;
@@ -62,16 +136,13 @@ void	cycle(t_cor *cor)
 	t_champ		*cur_champ;
 	int			*last_pc;
 	int			*last_champ;
-	char		*cycle;
-	int			ch;
 	int			timeout;
-	char		*p_p_c;
+	int			ch;
 
 	timeout = 950;
 	last_pc = (int*)ft_malloc(MAX_PLAYERS * sizeof(int), EXIT_FAILURE);
 	last_champ = (int*)ft_malloc(MAX_PLAYERS * sizeof(int), EXIT_FAILURE);
-	champs = cor->champs;
-	first_champ = champs;
+	first_champ = cor->champs;
 	while ((champs = first_champ))
 	{
 		// Clean cursor
@@ -82,6 +153,7 @@ void	cycle(t_cor *cor)
 			cur_champ = champs->content;
 			if (cur_champ->r_cy > -1)
 			{
+				// Do op or decrement r_cy
 				if (cur_champ->r_cy == 0)
 					cycle_job(cor, cur_champ, last_champ, last_pc);
 				else
@@ -90,74 +162,11 @@ void	cycle(t_cor *cor)
 			champs = champs->next;
 		}
 		// Print infos
-		cycle = ft_itoa((cor->cycle)++);
-		p_p_c = ft_itoa(1000 - timeout);
 		if (cor->opt->n)
-		{
-			attron(COLOR_PAIR(7));
-			draw_line(7, 9, cycle);
-			attron(COLOR_PAIR(7));
-			draw_line(4, 22, p_p_c);
-		}
-		free(p_p_c);
-		free(cycle);
+			print_infos(cor);
 		// Key event
 		if (cor->opt->n)
-		{
-			if (ch != ' ')
-			{
-				noecho();
-				ch = getch();
-				while (ch != 's' && ch != ' ')
-				{
-					if (ch == 'e' && timeout > 1)
-					{
-						p_p_c = ft_itoa(1000 - --timeout);
-						attron(COLOR_PAIR(7));
-						draw_line(4, 22, "    ");
-						draw_line(4, 22, p_p_c);
-						free(p_p_c);
-					}
-					else if (ch == 'r' && timeout > 1)
-					{
-						if (1000 - (timeout - 10) > 1000)
-							p_p_c = ft_itoa(1000 - (timeout = 0));
-						else
-							p_p_c = ft_itoa(1000 - (timeout -= 10));
-						attron(COLOR_PAIR(7));
-						draw_line(4, 22, "    ");
-						draw_line(4, 22, p_p_c);
-						free(p_p_c);
-					}
-					else if (ch == 'w' && timeout < 950)
-					{
-						p_p_c = ft_itoa(1000 - ++timeout);
-						attron(COLOR_PAIR(7));
-						draw_line(4, 22, "    ");
-						draw_line(4, 22, p_p_c);
-						free(p_p_c);
-					}
-					else if (ch == 'q' && timeout < 950)
-					{
-						if (1000 - (timeout + 10) < 50)
-							p_p_c = ft_itoa(1000 - (timeout = 950));
-						else
-							p_p_c = ft_itoa(1000 - (timeout += 10));
-						attron(COLOR_PAIR(7));
-						draw_line(4, 22, "    ");
-						draw_line(4, 22, p_p_c);
-						free(p_p_c);
-					}
-					ch = getch();
-				}
-				echo();
-			}
-			else
-			{
-				usleep(timeout);
-				refresh();
-			}
-		}
+			key_event(&timeout, &ch);
 	}
 	free(last_pc);
 	free(last_champ);
