946d0fa903cb3889efc8084ac5ec2b57ee142340
Pierre MILAN <pmilan@student.42.fr>
2018-06-18 16:44:54 +0200

    idx

--- corewar/srcs/op2.c
+++ corewar/srcs/op2.c
@@ -6,7 +6,7 @@
 /*   By: anhuang <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/13 15:06:07 by anhuang           #+#    #+#             */
-/*   Updated: 2018/06/15 16:55:30 by pmilan           ###   ########.fr       */
+/*   Updated: 2018/06/18 16:31:20 by pmilan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -116,7 +116,7 @@ void	ft_zjmp(t_cor *cor, t_champ *champ)
 	ori = champ->pc;
 	p = (cor->map[++champ->pc % MEM_SIZE] << 8) + cor->map[++champ->pc % MEM_SIZE];
 	if (champ->carry == 1)
-		champ->pc = (ori + p) % MEM_SIZE;
+		champ->pc = ((ori + p) % IDX_MOD) % MEM_SIZE;
 	else
 		champ->pc = (champ->pc + 1) % MEM_SIZE;
 }
