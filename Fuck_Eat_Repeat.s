60647d67074b87c2ec3db640f11dc27cc3d159f8
eparisot <eparisot@student.42.fr>
2018-06-26 17:31:48 +0200

    finished champ

--- Fuck_Eat_Repeat.s
+++ Fuck_Eat_Repeat.s
@@ -1,8 +1,6 @@
 .name		"Fuck, Eat, Repeat"
 .comment	"Basics..."
 
-fuck:			# duplicate champ 
-
 eat:			sti r1, %:repeat, %4			# names live
 				sti r1, %:eat, %15				# names live
 				live %42
@@ -13,13 +11,13 @@ eat:			sti r1, %:repeat, %4			# names live
 				sti r4, %115, r5				# build son's zjmp address
 				ld  %9, r3						# load zjmp opcode inr r3
 				sti r3, %100, r5				# build son's jump
-				ld %20, r6						# set son's offset
+				ld %25, r6						# set son's offset
 				add r5, r6, r5					# calc next son's address
 
 repeat:			fork	%:eat					# fork back to eat
 				live %42
 				sub r5, r6, r7					# catch the last son's adress
-				ld  %22, r8						# calc offset for next zjmp
+				ld  %25, r8						# calc offset for next zjmp
 				add r7, r8, r7					# calc address for next zjmp
 				sti r7, %:repeat, %53			# write address for next zjmp
 				ld %9, r9						# load jump opcode
