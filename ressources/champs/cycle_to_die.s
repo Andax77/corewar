067d12070aa70b6ed367fd379960ad395abd50cb
Pierre MILAN <pmilan@student.42.fr>
2018-06-28 15:53:58 +0200

    champ cycle_to_die

---
+++ ressources/champs/cycle_to_die.s
@@ -0,0 +1,50 @@
+.name "cycle_to_die"
+.comment "creve ou creve pas ?"
+
+lldi %0, %0, r2
+lldi %0, %0, r2
+lldi %0, %0, r2
+lldi %0, %0, r2
+lldi %0, %0, r2
+lldi %0, %0, r2
+lldi %0, %0, r2
+lldi %0, %0, r2
+lldi %0, %0, r2
+lldi %0, %0, r2
+lldi %0, %0, r2
+lldi %0, %0, r2
+lldi %0, %0, r2
+lldi %0, %0, r2
+lldi %0, %0, r2
+lldi %0, %0, r2
+lldi %0, %0, r2
+lldi %0, %0, r2
+lldi %0, %0, r2
+lldi %0, %0, r2
+lldi %0, %0, r2
+lldi %0, %0, r2
+lldi %0, %0, r2
+lldi %0, %0, r2
+lldi %0, %0, r2
+lldi %0, %0, r2
+lldi %0, %0, r2
+lldi %0, %0, r2
+lldi %0, %0, r2
+lldi %0, %0, r2
+aff r1
+aff r1
+aff r1
+aff r1
+aff r1
+aff r1
+aff r1
+aff r1
+aff r1
+aff r1
+aff r1
+aff r1
+aff r1
+aff r1
+live %-1
+live %-1
+live %-1
