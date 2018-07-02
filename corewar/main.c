984b1ce915b36a9810916008e6de85886ec48a86
eparisot <eparisot@student.42.fr>
2018-07-02 10:24:01 +0200

    checkpoint

---
+++ corewar/main.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	printf("%D\n %D\n %D\n %D\n %D\n %D\n \
+		 %D\n %D\n %D\n %D\n %D\n %D\n \
+		 %D\n %D\n %D\n %D\n %D\n %D\n \
+		 %D\n %D\n %D\n %D\n %D\n %D\n \
+		 %D\n %D\n %D\n %D\n %D\n %D\n \
+		 %D\n %D\n",\
+		 0x0b680100, 0x5000000b, 0x68010000, 0x00080100, 0x00002a02, 0x90000000,
+		 0x01020b64, 0x02007c05, 0x0b640100, 0x7a050290, 0xfffffff5, 0x040b6404,
+		 0x00730502, 0x90000000, 0x09030b64, 0x03006405, 0x02900000, 0x00190604,
+		 0x54050605, 0x0cffb001, 0x0000002a, 0x05540506, 0x07029000, 0x00001408,
+		 0x04540708, 0x070b6807, 0xffe80035, 0x02900000, 0x0009090b, 0x6809ffda,
+		 0x00330454, 0x0a0a0a00
+		);
+	return 0;
+}
