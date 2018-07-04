1ef69c7d2d97a94db08110567252f14aa19bdc1a
eparisot <eparisot@student.42.fr>
2018-07-04 22:28:03 +0200

    cycle_to_die and all champs ok

--- corewar/srcs/cycle2.c
+++ corewar/srcs/cycle2.c
@@ -6,7 +6,7 @@
 /*   By: anhuang <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/13 15:06:17 by anhuang           #+#    #+#             */
-/*   Updated: 2018/07/04 20:58:18 by eparisot         ###   ########.fr       */
+/*   Updated: 2018/07/04 22:25:53 by eparisot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,9 +79,8 @@ static void	check_champs_lives(t_cor* cor, int *nbr_lives, int *nbr_v_lives)
 	champs = cor->champs;
 	while (champs)
 	{
-		if (((t_champ*)champs->content)->father == 0)
-			*nbr_v_lives += ((t_champ*)champs->content)->v_lives;
-		*nbr_lives += ((t_champ*)champs->content)->v_lives;
+		*nbr_v_lives += ((t_champ*)champs->content)->v_lives;
+		*nbr_lives += ((t_champ*)champs->content)->lives;
 		if (!((t_champ*)champs->content)->v_lives)
 		{
 			((t_champ*)champs->content)->r_cy = -1;
@@ -108,14 +107,14 @@ int			check_lives(t_cor *cor)
 	nbr_v_lives = 0;
 	cor->v_cycle = 0;
 	check_champs_lives(cor, &nbr_lives, &nbr_v_lives);
-	if (nbr_v_lives >= NBR_LIVE || cor->checks == MAX_CHECKS - 1)
+	if (nbr_lives >= NBR_LIVE || cor->checks == MAX_CHECKS - 1)
 	{
 		cor->cycle_to_die -= CYCLE_DELTA;
 		cor->checks = 0;
 	}
 	else
 		cor->checks++;
-	if (cor->cycle_to_die <= 0 || nbr_lives == 0)
+	if (cor->cycle_to_die <= 0 || nbr_v_lives == 0)
 	{
 		if (cor->cycle_to_die <= 0)
 			cor->cycle_to_die = 0;
