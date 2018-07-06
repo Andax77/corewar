1692b156b83829dcda5d8e7fb0e043db5a87b67c
eparisot <eparisot@student.42.fr>
2018-07-06 16:12:02 +0200

    champ cleaned if dead

--- corewar/srcs/cycle.c
+++ corewar/srcs/cycle.c
@@ -6,7 +6,7 @@
 /*   By: anhuang <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/13 15:06:17 by anhuang           #+#    #+#             */
-/*   Updated: 2018/07/06 13:15:48 by eparisot         ###   ########.fr       */
+/*   Updated: 2018/07/06 16:09:58 by eparisot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,9 +66,9 @@ static void	exec_processes(t_cor *cor, t_list *champs, void (**f)(t_cor*,
 		cur_champ = champs->content;
 		if (cur_champ->r_cy > -1)
 			cycle_job(cor, cur_champ, f);
-		else if (cur_champ->r_cy <= -1)
+		else if (cur_champ->r_cy < -1)
 		{
-			if (cor->opt->v)
+			if (cor->opt->v && !cor->opt->d)
 			{
 				attron(COLOR_PAIR(cur_champ->id + 20));
 				draw_uchar(cur_champ->pc, cor->map[cur_champ->pc]);
