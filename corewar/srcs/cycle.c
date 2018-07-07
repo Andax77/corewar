91c914f89cf9626586aa7a48ced776501f5ee0f5
eparisot <eparisot@student.42.fr>
2018-07-07 14:34:40 +0200

    woup processes fixed

--- corewar/srcs/cycle.c
+++ corewar/srcs/cycle.c
@@ -6,7 +6,7 @@
 /*   By: anhuang <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/13 15:06:17 by anhuang           #+#    #+#             */
-/*   Updated: 2018/07/07 12:53:34 by eparisot         ###   ########.fr       */
+/*   Updated: 2018/07/07 14:34:24 by eparisot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,7 +74,14 @@ static void	exec_processes(t_cor *cor, t_list *champs, void (**f)(t_cor*,
 	{
 		cur_champ = champs->content;
 		if (cur_champ->r_cy > -1)
+		{
 			cycle_job(cor, cur_champ, f);
+			if (cor->opt->v && !cor->opt->d)
+			{
+				attron(COLOR_PAIR(cur_champ->id + 20));
+				draw_uchar(cur_champ->pc, cor->map[cur_champ->pc]);
+			}
+		}
 		champs = champs->next;
 	}
 	if (cor->opt->v && !cor->opt->d)
