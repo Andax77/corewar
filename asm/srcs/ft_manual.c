fa70e6fbf4f60d4a9f2a4aae0609c2a6abb7b526
Pierre MILAN <pmilan@student.42.fr>
2018-07-10 14:02:22 +0200

    leak nom programme vide fix

--- asm/srcs/ft_manual.c
+++ asm/srcs/ft_manual.c
@@ -6,7 +6,7 @@
 /*   By: pierremilan <marvin@42.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/16 20:20:15 by pmilan            #+#    #+#             */
-/*   Updated: 2018/06/29 19:02:12 by pmilan           ###   ########.fr       */
+/*   Updated: 2018/07/10 14:01:09 by pmilan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -100,9 +100,14 @@ sizeof(instructions_length) + COMMENT_LENGTH + 4 + instructions_length;
 	ft_fill_output_m(champ, output, f_instructions, instructions_length);
 	ft_printf("{yellow}What is the name of the program (please finish the name \
 with '.cor') ?{eoc}\n");
-	free(champ->file_name);
+	ft_fruit(1, &champ->file_name);
 	if (get_next_line(STDIN_FILENO, &champ->file_name) != GNL_SUCCESS)
 		return (ft_error_m(champ, "error: oups something went wrong..."));
+	if (!champ->file_name || champ->file_name[0] == '\0')
+	{
+		ft_fruit(1, &output);
+		return (ft_error_m(champ, "error: oups something went wrong..."));
+	}
 	if ((fd = open(champ->file_name, O_CREAT | O_RDWR | O_TRUNC, S_IRWXU)) <= 0)
 		exit(EXIT_FAILURE);
 	write(fd, output, length_output);
@@ -113,10 +118,10 @@ with '.cor') ?{eoc}\n");
 
 int			ft_manual(t_champ *champ)
 {
-	char	*f_inst;
+	char	*inst;
 	int		inst_len;
 
-	f_inst = NULL;
+	inst = NULL;
 	inst_len = 0;
 	ft_printf("{yellow}What is the champion's name ?{eoc}\n");
 	if (get_next_line(STDIN_FILENO, &champ->name) != GNL_SUCCESS)
@@ -128,14 +133,14 @@ int			ft_manual(t_champ *champ)
 		return (ft_error_m(champ, "error: oups something went wrong..."));
 	if (ft_strlen(champ->comment) > COMMENT_LENGTH)
 		return (ft_error_m(champ, "error: comment is too long"));
-	if (ft_fill_instructions_m(champ, &f_inst, &inst_len) == ERROR)
+	if (ft_fill_instructions_m(champ, &inst, &inst_len) == ERROR)
 		return (ERROR);
-	if (ft_write_cor_m(champ, inst_len, f_inst) == ERROR)
+	if (ft_write_cor_m(champ, inst_len, inst) == ERROR && ft_fruit(1, &inst))
 		return (ERROR);
 	del_champ(champ);
-	ft_printf("{yellow}Please press ^d{eoc}\n") && ft_fruit(1, &f_inst);
-	while (get_next_line(STDIN_FILENO, &f_inst) != GNL_END)
-		ft_printf("{yellow}Please press ^d{eoc}\n") && ft_fruit(1, &f_inst);
-	free(f_inst);
+	ft_printf("{yellow}Please press ^d{eoc}\n") && ft_fruit(1, &inst);
+	while (get_next_line(STDIN_FILENO, &inst) != GNL_END)
+		ft_printf("{yellow}Please press ^d{eoc}\n") && ft_fruit(1, &inst);
+	free(inst);
 	return (SUCCESS);
 }
