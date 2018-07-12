5ff84087f608db0874a986b8df9d110504fc5f8a
Pierre MILAN <pmilan@student.42.fr>
2018-07-12 17:32:49 +0200

    fonction commune change_p1_p2

--- corewar/srcs/op_and_or_xor.c
+++ corewar/srcs/op_and_or_xor.c
@@ -6,37 +6,12 @@
 /*   By: pmilan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/05 20:22:19 by pmilan            #+#    #+#             */
-/*   Updated: 2018/07/12 17:09:42 by pmilan           ###   ########.fr       */
+/*   Updated: 2018/07/12 17:32:43 by pmilan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <corewar.h>
 
-//static int	ft_change_p1_p2(t_champ *champ, int ocp, int *p1, int *p2)
-//{
-//	if (((ocp >> 6) & 3) == REG_CODE)
-//	{
-//		if (*p1 > 0 && *p1 <= REG_NUMBER)
-//			*p1 = champ->reg[*p1 - 1];
-//		else
-//		{
-//			champ->pc = (champ->pc + 1) % MEM_SIZE;
-//			return (ERROR);
-//		}
-//	}
-//	if (((ocp >> 4) & 3) == REG_CODE)
-//	{
-//		if (*p2 > 0 && *p2 <= REG_NUMBER)
-//			*p2 = champ->reg[*p2 - 1];
-//		else
-//		{
-//			champ->pc = (champ->pc + 1) % MEM_SIZE;
-//			return (ERROR);
-//		}
-//	}
-//	return (SUCCESS);
-//}
-//
 void		ft_and(t_cor *cor, t_champ *champ)
 {
 	int		p1;
